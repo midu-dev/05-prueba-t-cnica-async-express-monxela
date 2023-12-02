1ab97b47e317bd2a9ff7074c33b2522418e24159
Ramón A. Mediavilla <ramon.mediavilla@ironia.tech>
2023-12-02 20:27:23 +0100

    Fix obtenerDatosPromise

--- solutions/index.js
+++ solutions/index.js
@@ -22,10 +22,10 @@ ping('midu.dev', (err, info) => {
 })
 
 // # EJERCICIO 2
-export function obtenerDatosPromise (callback) {
-  setTimeout(() => {
-    callback(null, { data: 'datos importantes' })
-  }, 2000)
+export function obtenerDatosPromise () {
+  return new Promise(resolve => {
+    setTimeout(resolve({ data: 'datos importantes' }), 2000)
+  })
 }
 
 // # EJERCICIO 3
